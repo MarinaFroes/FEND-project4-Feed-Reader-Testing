a7e239d27b60afeb7276c874f68124c9b2d2c75d
MFAC <ninafroes@gmail.com>
2019-05-08 09:30:26 +0200

    feat

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -2,7 +2,6 @@
  */
 
 (() => {
-
     describe('RSS Feeds', () => {
         /* Test to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -18,7 +17,7 @@
          * and that the URL is not empty.
          */
         it('urls are defined and not empty', () => {
-            allFeeds.map(feed => {
+            allFeeds.forEach(feed => {
                 expect(feed.url).toBeDefined();
                 expect(feed.url).not.toBe('');
             });
@@ -29,7 +28,7 @@
          * and that the name is not empty.
          */
         it('names are defined and not empty', () => {
-            allFeeds.map(feed => {
+            allFeeds.forEach(feed => {
                 expect(feed.name).toBeDefined();
                 expect(feed.name).not.toBe('');
             });
@@ -37,12 +36,12 @@
     });
 
     describe('The menu', () => {
-
+        const menu = document.querySelector('body');
         /* Test that ensures the menu element is
          * hidden by default. 
          */
         it('is hidden by default', () => {
-            expect(document.querySelector('body')).toHaveClass('menu-hidden');
+            expect(menu).toHaveClass('menu-hidden');
         });
         /* Test that ensures the menu changes
         * visibility when the menu icon is clicked.
@@ -50,9 +49,9 @@
         it('is displayed on click', () => {
             const menuIcon = document.querySelector('.menu-icon-link');
             menuIcon.click();
-            expect(document.querySelector('body')).not.toHaveClass('menu-hidden');
+            expect(menu).not.toHaveClass('menu-hidden');
             menuIcon.click();
-            expect(document.querySelector('body')).toHaveClass('menu-hidden');
+            expect(menu).toHaveClass('menu-hidden');
         });
     });
 
@@ -72,24 +71,22 @@
     });
 
     describe('New Feed Selection', () => {
-        let firstFeed, secondFeed;
+        let initialFeed, finalFeed;
         /* Test that ensures when a new feed is loaded
         * by the loadFeed function that the content actually changes.
-        * Remember, loadFeed() is asynchronous.
         */
         beforeEach( done => {
             loadFeed(0, () => {
-                firstFeed = document.querySelector('.feed').innerHTML;
-                done();
-            });
-            loadFeed(1, () => {
-                secondFeed = document.querySelector('.feed').innerHTML;
-                done();
+                initialFeed = document.querySelector('.feed').innerText;
+                loadFeed(1, () => {
+                    finalFeed = document.querySelector('.feed').innerText;
+                    done();
+                });
             });
         });
 
         it('changes content when new feed is loaded', () => {
-            expect(firstFeed !== secondFeed).toBe(true);
+            expect(initialFeed).not.toBe(finalFeed);
         });
     });
 })();
